3707344704ded33450f66aed42c52f3251582540
pkprosol <peter.prosol@gmail.com>
2016-12-19 16:17:09 -0500

    Initial data

--- src/App.js
+++ src/App.js
@@ -14,16 +14,33 @@ class App extends Component {
       //     To get started, edit <code>src/App.js</code> and save to reload.
       //   </p>
       // </div>
-      <div>
-        <div className="Question-text">
-          Between the two top brands of toothpaste, which is a better choice for most people: Crest Pro-Health or Colgate Total?
-        </div>
-        <div className="Answer-array">
-          <ol>
-            <li>
-              Crest, because Colgate is imported from North Korea.
-            </li>
-          </ol>
+      <div className="Quiz-container">
+        <div className="Question-and-answer-container">
+          <h1 className="Question-text">
+            Between the two top brands of toothpaste, which is a better choice for most people: Crest Pro-Health or Colgate Total?
+          </h1>
+          <p className="Instruction-text">
+            Select 1 Answer
+          </p>
+          <div className="Answer-array">
+            <ol>
+              <li>
+                Crest, because Colgate is imported from North Korea.
+              </li>
+              <li>
+                Crest, because it comes in a shinier tube.
+              </li>
+              <li>
+                Whichever brand is on sale, of course.
+              </li>
+              <li>
+                Crest, because Colgate contains a chemical that the government has banned from soap and other personal care products.
+              </li>
+              <li>
+                Colgate, because it has more fluoride than Crest.
+              </li>
+            </ol>
+          </div>
         </div>
       </div>
     );
