237f56ca57b8fae83bfc298ff9d85398be734b64
pkprosol <peter.prosol@gmail.com>
2017-01-23 17:36:57 -0500

    Attempting IE fix

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.6.15/browser-polyfill.min.js"></script>
         <meta name="viewport" content="initial-scale=1, maximum-scale=1" />
         <Quiz />
       </div>
